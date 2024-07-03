fe3e4957ef5d48521ce5dbe0731f25bc65014554
Himalaya7087 <himalayasingh.hs@gmail.com>
2024-07-03 22:02:38 +0530

    scripts

---
+++ scripts/before_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the directory path
+DIR="/var/www/node-app"
+
+# Check if the directory exists
+if [ -d "$DIR" ]; then
+    echo "Directory $DIR exists. Removing all files in it..."
+    rm -rf ${DIR:?}/*
+else
+    echo "Directory $DIR does not exist. Creating the directory..."
+    mkdir -p "$DIR"
+fi
+
+echo "Operation completed."
